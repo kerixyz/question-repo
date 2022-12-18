2911746a6b6905b2d9da9eb7cbc7862831da904a
Keri <asteria@Keris-MacBook-Pro-2.local>
2022-12-18 04:02:28 -0800

    what am i doing

--- src/components/NavBar/NavbarElements.js
+++ src/components/NavBar/NavbarElements.js
@@ -1,4 +1,4 @@
-import { FaBars } from "react-icons/fa";
+// import { FaBars } from "react-icons/fa";
 import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -7,4 +7,40 @@ export const Nav = styled.nav`
     height: 100px;
     display: flex;
     justify-content: space-between;
+`;
+
+export const NavLink = styled(Link)`
+    color: #808080;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+    &.active {
+        color: #4d4dff;
+        }
+`;
+
+// export const Bars = styled(FaBars)`
+//     display: none;
+//     color: #808080;
+//     @media screen and (max-width: 768px) {
+//         display: block;
+//         position: absolute;
+//         top: 0;
+//         right: 0;
+//         transform: translate(-100%, 75%);
+//         font-size: 1.8rem;
+//         cursor: pointer;
+//     }
+// `;
+
+export const NavMenu = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: -24px;
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
 `;
